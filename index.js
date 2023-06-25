8336c2a9a41d2a252f6529b1f241e8fb251e4d93
Arjun7752 <82261529+Arjun7752@users.noreply.github.com>
2023-06-25 21:45:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 RXjs ( Reactive programming)
 SOLID princeples
-JSON server (https://www.youtube.com/watch?v=yz2t40vWtZg&list=PLC3y8-rFHvwhc9YZIdqNL5sWeTCGxF4ya&index=6 )
-
+JSON server (https://www.youtube.com/watch?v=yz2t40vWtZg&list=PLC3y8-rFHvwhc9YZIdqNL5sWeTCGxF4ya&index=6)
+Tree Shaking in javascript
+Microfront end architecture
+JWT/Open ID/Oauth 2
+SOLID Principles
